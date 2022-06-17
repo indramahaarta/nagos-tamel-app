1562ba9e743b3efdde47679ca2d62cf9f68614c9
indramahaarta <official.indramahaarta@gmail.com>
2022-06-18 06:30:44 +0700

    first commit

---
+++ src/main/java/com/code/nagostamelapp/NagosTamelAppApplication.java
@@ -0,0 +1,13 @@
+package com.code.nagostamelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagosTamelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagosTamelAppApplication.class, args);
+    }
+
+}
